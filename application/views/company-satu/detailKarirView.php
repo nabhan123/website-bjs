6284453966ddf0c2c85590326521de3a15124bfc
Alvian Akmal Nabhan <alvianakmaln@gmail.com>
2024-01-18 22:08:14 +0700

    developing extend

---
+++ application/views/company-satu/detailKarirView.php
@@ -0,0 +1,48 @@
+<div class="container">
+                <div class="row" style="padding-top:15px;"> 
+                    <div class="col-md-9">
+                        <div class="blog-post single-post">
+                            <article class="post blog-single-post">
+                                <div>
+								
+									<div class="post-image">
+                                        <div class="owl-carousel owl-theme" data-plugin-options="{'items':1}">
+                                            <div>
+                                                <h1><?= $title?></h1>
+                                                
+    
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <aside class="siderbar">
+							<h4 class="heading-primary">Latest News</h4>
+                            <ul class="nav nav-list mb-xlg">
+							
+                            </ul>
+                            <hr />
+							
+                       
+                        </aside>
+                    </div>
+                </div>
+            </div>
+       
+
+<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b878e417c0d681d"></script>
+
+
+
+
+
+
+
+
+
